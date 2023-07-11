d49084dd380eae758a1f2df8e8074a9c109d3697
gemaTaufik <gemataufik19@gmail.com>
2023-07-12 00:57:22 +0700

    tugas 24

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt');
 
 
 module.exports = (pool) => {
@@ -16,9 +17,19 @@ router.get('/', function (req, res, next) {
   });
 });
 
+
 router.get('/add', (req, res, next) => {
 
-  res.render('adduser', { title: 'Add User' });
+  pool.query('SELECT * FROM users', (err, result) => {
+    if (err) {
+      console.error('Error executing query', err);
+      res.render('error', { message: 'Error retrieving users' });
+    } else {
+      res.render('adduser', { title: 'Express', 
+      data: result.rows, 
+   renderFrom : "add" });
+    }
+  });
 });
 
 router.post('/add', function (req, res, next) {
@@ -37,6 +48,38 @@ router.post('/add', function (req, res, next) {
   });
 });
 
+router.get('/edit/:userid', function (req, res, next) {
+  const userid = req.params.userid;
+  const name = req.session.user?.name;
+
+  pool.query('SELECT * FROM users WHERE userid = $1', [userid], (error, result) => {
+    if (error) {
+      console.error('Error retrieving user data:', error);
+    } else {
+      const user = result.rows[0];
+      res.render('adduser', { title: 'Edit User', data: user, name: name, renderFrom : "edit"});
+    }
+  });
+});
+
+router.post('/edit/:userid', (req, res, next) => {
+  const userId = req.params.userid
+  const { email, name, password, role } = req.body
+
+  pool.query(
+    'UPDATE users SET email = $1, name = $2, password = $3, role = $4 WHERE userid = $5',
+    [email, name, password, role, userId],
+    (error, result) => {
+      if (error) {
+        console.error('Error updating user data:', error);
+      } else {
+        res.redirect('/users');
+      }
+    }
+  )
+})
+
+
 router.get('/delete/:id', function (req, res, next) {
   var userId = req.params.id;
 
@@ -45,7 +88,7 @@ router.get('/delete/:id', function (req, res, next) {
       console.error('Error executing query', err);
       res.render('error', { message: 'Error deleting user' });
     } else {
-      res.redirect('/users'); // Redirect ke halaman users setelah berhasil menghapus data
+      res.redirect('/users'); 
     }
   });
 });
